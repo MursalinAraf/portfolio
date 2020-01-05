854355023a05e3e72fc57d89cdf0116e1efa0bc4
unknown <mursalinaraf1@gmail.com>
2020-01-05 23:32:38 +0600

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,146 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Syed Mursalin Rahman Araf",
+    "role": "Frontend Developer",
+    "linkedinId":"https://www.linkedin.com/in/syed-mursalin-rahman-330122142/",
+    "facebookId" : "https://www.facebook.com/mursalin.araf",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time. Currently I am learning backend technologies to become a full-stack developer",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/syed-mursalin-rahman-330122142/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/mursalinaraf",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"fscebook",
+          "url":"http://facebook.com/mursalin.araf",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"I am a BSc graduate from Rajshahi University of Engineering & Technology. I am a self taught Front End Web Developer, currently diving deeper into Backend Developing to become a Full Stack developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Dhaka, Bangladesh",
+    "phone" : "01758585137",
+    "website":"https://github.com/mursalinaraf",
+    "education":[
+      {
+        "UniversityName":"Rajshahi University of Engineering & Technology",
+        "specialization":"Bachelor of Science in CSE",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2019",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Pran RFL Group",
+        "specialization":"Worked full time as Junior Software Engineer",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2019",
+        "Achievements":"Worked with JavaScript and React"
+      },
+      {
+        "CompanyName":"Optima Solution",
+        "specialization":"Worked as an intern",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2019",
+        "Achievements":"Worked with PHP and Laraval"
+      }
+    ],
+    "skillsDescription":"My skills",
+    "skills":[
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Bootstrap 4"
+      },
+      {
+        "skillname":"JQUERY"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"C/C++"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Movie Finder",
+        "description":"Web App",
+        "imgurl":"images/portfolio/movie3.jpg"
+      },
+      {
+        "name":"Restaurant Finder",
+        "description":"Web App",
+        "imgurl":"images/portfolio/rest.jpg"
+      },
+      {
+        "name":"Weather App",
+        "description":"Web App",  
+        "imgurl":"images/portfolio/weather.jpg"
+      },
+      {
+        "name":"Rock-Paper-Scissor",
+        "description":"Web App",
+        "imgurl":"images/portfolio/rsp.jpg"
+      },
+      {
+        "name":"Meditation",
+        "description":"Web App",
+        "imgurl":"images/portfolio/medi.jpg"
+      },
+      {
+        "name":"Almira Collection",
+        "description":"Web App",
+        "imgurl":"images/portfolio/almira.jpg"
+      },
+      {
+        "name":"Budget Calculation",
+        "description":"Web App",
+        "imgurl":"images/portfolio/budget.jpg"
+      },
+      {
+        "name":"Vehicle Management",
+        "description":"https://github.com/MursalinAraf/VehicleManagement",
+        "imgurl":"images/portfolio/vehicle.jpg"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"When you reach the end of your rope, tie a knot in it and hang on.",
+        "name":"Franklin D. Roosevelt"
+      },
+      {
+        "description":"Do not mind anything that anyone tells you about anyone else. Judge everyone and everything for yourself.",
+        "name":"Henry James"
+      },
+      {
+        "description":"Do right, Do your best. Treat others as you want to be treated",
+        "name":"Lou Holtz"
+      },
+      {
+        "description":"The best way to cheer yourself up is to try to cheer somebody else up",
+        "name":"Mark Twain"
+      },
+    ]
+  }
+  
+  export default resumeData
